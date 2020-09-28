78ad4c744eaa3b9ccd8e8e8ffdb59e067f3dabfd
shao <353124505@qq.com>
2020-09-28 22:42:08 +0800

    about

--- src/lang/zh.js
+++ src/lang/zh.js
@@ -7,5 +7,13 @@ export default {
     carousel:{
         shopping_name:"大兵健身器材南通有限公司网上商城",
         welcome:"欢迎您！"
+    },
+    about:{
+        title:"大兵健身器材南通有限公司",
+        address:"地址：江苏省如皋市白蒲镇姚园村十九组",
+        phone:"客服热线：0513-87885889",
+        website:"网址(Web)：www.dbfitness.cn",
+        fax:"公司传真：0513-87885889",
+        email:"客服邮箱：520144479@qq.com"
     }
 };
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,42 @@
 <template>
   <div class="about">
-<!--    <div class="container">-->
-<!--      <div class="modal-content text-center">-->
-<!--        <div class="modal-header">-->
-<!--          <h4 class="modal-title" style="margin: 0 auto">-->
-<!--            More About-->
-<!--            <span style="color:crimson">大兵网上商城</span>-->
-<!--          </h4>-->
-<!--        </div>-->
-<!--        <div class="modal-body">-->
+    <div class="container">
+      <div class="modal-content text-center">
+        <div class="modal-header">
+          <h4 class="modal-title" style="margin: 0 auto">
+            <strong>{{$t("about.title")}}</strong>
+<!--            <span style="color:crimson">{{$t("about.title")}}</span>-->
+          </h4>
+        </div>
+          <div class="modal-body">
+            视质量为企业生命，狠抓产品质量，以最大可能为客户做到最好！公司自主生产、加工、销售的各种产品，满足客户对产品的需求和各种要求。
+              我们深信，别人做不到的，我们能做到；别人做得到的，我们将会做得更好。</div>
+        </div>
+        <div class="more_information">
+            <div class="row font">
+                <div class="col">
+                    {{$t("about.address")}}
+                </div>
+                <div class="col">
+                    {{$t("about.phone")}}
+                </div>
+                <div class="col">
+                    {{$t("about.website")}}
+                </div>
+            </div>
+            <div class="row font">
+                <div class="col">
+                    {{$t("about.fax")}}
+                </div>
+                <div class="col">
+                    {{$t("about.email")}}
+                </div>
+                <div class="col">
+
+                </div>
+            </div>
+    </div>
+    </div>
 <!--          <img-->
 <!--            src="https://avatars1.githubusercontent.com/u/17190913?s=460&v=4"-->
 <!--            name="aboutme"-->
@@ -31,22 +59,28 @@
 <!--          <span class="badge badge-secondary p-2 m-1">React</span>-->
 <!--          <span class="badge badge-success p-2 m-1">Github</span>-->
 <!--          <span class="badge badge-danger p-2 m-1">RestService</span>-->
-<!--          &lt;!&ndash; <hr> &ndash;&gt;-->
-<!--          &lt;!&ndash; <p class="text-left">-->
-<!--            <strong>Bio:</strong>-->
-<!--            <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem dui, tempor sit amet commodo a, vulputate vel tellus.-->
-<!--          </p>-->
-<!--          <br>&ndash;&gt;-->
-<!--        </div>-->
-<!--        &lt;!&ndash; <div class="modal-footer" style="margin: 0 auto">-->
-<!--          <button type="button" class="btn btn-default" data-dismiss="modal">Read More</button>-->
-<!--        </div>&ndash;&gt;-->
-<!--      </div>-->
-<!--    </div>-->
+          <!-- <hr> -->
+          <!-- <p class="text-left">
+            <strong>Bio:</strong>
+            <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem dui, tempor sit amet commodo a, vulputate vel tellus.
+          </p>
+          <br>-->
+
+        <!-- <div class="modal-footer" style="margin: 0 auto">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Read More</button>
+        </div>-->
   </div>
 </template>
 <style>
 .avatar {
   border-radius: 10px;
 }
+    .about .font{
+        font-size: 14px;
+        text-align:left;
+    }
+
+    .about .more_information{
+      line-height: 2;
+    }
 </style>
