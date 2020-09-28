b7e347a70e51662edfa669b528fa89c6c1b44d96
allen_shao <Allen_Shao@trendmicro.com>
2020-09-28 17:46:31 +0800

    test

--- src/components/products/BestProducts.vue
+++ src/components/products/BestProducts.vue
@@ -32,20 +32,21 @@ export default {
     },
 
     getBestProducts() {
-      this.loading = true;
-      axios
-        .get(`${process.env.VUE_APP_BASE_URL}/best/products`)
-        .then(response => {
-          this.loading = false;
-          this.bestProducts = response.data;
-        })
-        .catch(error => {
-          console.log(error);
-          errorToaster(
-            "The server encountered an unexpected condition.",
-            "Internal Error 500"
-          );
-        });
+      this.bestProducts=[{"_id":"5bd44827429e8654fc8e5a4c","productName":"RealMe 1 (Black, 6GB RAM, 128GB Storage)","productPrice":"11,999.00","productCategory":"Smartphone","productImage":"https://images-na.ssl-images-amazon.com/images/I/616-sGx5LTL._SY879_.jpg","productSeller":"Realme","productRating":4.9,"isBestProduct":true,"isTopProduct":true,"__v":0,"productDescription":"Camera: 13 MP Rear camera with Fast facial unlock in less than 0.1 second with AI Recognition | 8 MP front camera Display: 15.24 centimeters (6-inch) Full HD In-cell capacitive touchscreen display with 2160x1080 pixels"},{"_id":"5bd4640589d37528b8eba255","productName":"Samsung Galaxy Note 8 (Midnight Black, 6GB RAM, 64GB Storage)","productPrice":"43,990.00","productCategory":"Smartphone","productImage":"https://images-na.ssl-images-amazon.com/images/I/815Aay6NVhL._SY879_.jpg","productSeller":"Samsung","productRating":4.5,"isBestProduct":true,"isTopProduct":false,"__v":null,"productDescription":"Camera: 12+12 MP Dual rear Camera with Autofocus, 2x Optical zoom | 8 MP front camera with Image recording, Touch focus and Face smile detection Display: 16.05 centimeters (6.3-inch) QHD+ capacitive touchscreen display with 1440x2960 pixels, 521 ppi pixel density and 18.5:9 aspect ratio"},{"_id":"5bd4659e89d37528b8eba256","productName":"Samsung Galaxy On7 Prime (Black, 4GB RAM, 64GB Storage)","productPrice":"10,990.00","productCategory":"Smartphone","productImage":"https://images-na.ssl-images-amazon.com/images/I/61EM4uS-5iL._SY879_.jpg","productSeller":"Samsung","productRating":3.5,"isBestProduct":true,"isTopProduct":true,"__v":null,"productDescription":"Camera: 13 MP Rear camera with f1.9 with Auto focus and Rear flash | 13 MP front camera Display: 13.88 centimeters (5.5-inch) Full HD display with 1080x1920 pixels | 2.5D Gorilla Glass Memory, Storage & SIM: 4GB RAM | 64GB storage expandable up to 256GB | Dual nano SIM with dual standby (4G+3G)"},{"_id":"5bd4712c89d37528b8eba25a","productName":"Apple iPhone X (Space Grey, 3GB RAM, 64GB Storage)","productPrice":"74,999.00","productCategory":"Smartphone","productImage":"https://images-na.ssl-images-amazon.com/images/I/61682XpNGFL._SY879_.jpg","productSeller":"Apple","productRating":5,"isBestProduct":true,"isTopProduct":true,"__v":null,"productDescription":"Camera: 12+12 MP Dual rear camera with Dual optical image stabilization, Portrait Mode, Digital zoom up to 10x, Quad-LED True tone flash and Slow sync, 4K Video recording at 24 fps or 30 fps or 60 fps and Slow-motion video recording in 1080p at 120 fps | 7 MP front TrueDepth camera with Retina Flash and Animoji feature"}];
+      // this.loading = true;
+      // axios
+      //   .get(`${process.env.VUE_APP_BASE_URL}/best/products`)
+      //   .then(response => {
+      //     this.loading = false;
+      //     this.bestProducts = response.data;
+      //   })
+      //   .catch(error => {
+      //     console.log(error);
+      //     errorToaster(
+      //       "The server encountered an unexpected condition.",
+      //       "Internal Error 500"
+      //     );
+      //   });
     }
   },
   created() {
